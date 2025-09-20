ca10a04827338b17564755612b798dc20c33d391
Qwrizi <abzalilovadel536@gmail.com>
2025-09-21 02:02:04 +0300

    Update: added new feature

--- bot/config.py
+++ bot/config.py
@@ -7,5 +7,5 @@ class Config:
     BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
     DATABASE_URL = 'sqlite:///birthdays.db'
     
-    REMINDER_HOUR = 2  # В 9 утра
+    REMINDER_HOUR = 2 
     REMINDER_MINUTE = 0
